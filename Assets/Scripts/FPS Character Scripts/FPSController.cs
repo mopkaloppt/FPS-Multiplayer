74fbd84f9b61fccf11b29256f493d7736171f2ef
mopkaloppt <moppi.sgotmoney@gmail.com>
2022-06-22 16:58:38 +0200

    Add MoveCameraCrouch
    and other related functions i.e. CanGetUp()

--- Assets/Scripts/FPS Character Scripts/FPSController.cs
+++ Assets/Scripts/FPS Character Scripts/FPSController.cs
@@ -29,6 +29,12 @@ public class FPSController : MonoBehaviour
     private CharacterController charController;
     private Vector3 moveDirection = Vector3.zero; 
 
+    public LayerMask groundLayer;
+    private float rayDistance;
+    private float default_ControllerHeight;
+    private Vector3 default_CamPos;
+    private float camHeight;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -37,6 +43,10 @@ public class FPSController : MonoBehaviour
         charController = GetComponent<CharacterController>();
         speed = walkSpeed;
         is_Moving = false;
+
+        rayDistance = charController.height * 0.5f + charController.radius;
+        default_ControllerHeight = charController.height;
+        default_CamPos = firstPerson_View.localPosition;
     }
 
     // Update is called once per frame
@@ -87,17 +97,93 @@ public class FPSController : MonoBehaviour
 
         if (is_Grounded)
         {
+            PlayerCrouchingAndSprinting();
+
             moveDirection = new Vector3(
                 inputX * inputModifyFactor, 
                 -antiBumpFactor, // smooth the player's movement when bumping into things
                 inputY * inputModifyFactor);
             // Transform the direction from local coord to Unity's world coord
             moveDirection = transform.TransformDirection(moveDirection) * speed;
+
+            // TODO Call Jump
         }
         // Character Controller does not apply gravity like Rigidbody so we need to apply it on our own
         moveDirection.y -= gravity * Time.deltaTime;
 
         is_Grounded = (charController.Move(moveDirection * Time.deltaTime) & CollisionFlags.Below) != 0;
         is_Moving = charController.velocity.magnitude > 0.15f;
+    }
+
+    void PlayerCrouchingAndSprinting()
+    {
+         if (Input.GetKeyDown(KeyCode.C))
+         {
+            if (!is_Crouching)
+            {
+                is_Crouching = true;
+            }
+            else
+            {
+                if (CanGetUp())
+                {
+                    is_Crouching = false;
+                }
+            }
+            StopCoroutine(MoveCameraCrouch());
+            StartCoroutine(MoveCameraCrouch());
+         }
+
+         if (is_Crouching)
+         {
+            speed = crouchSpeed;
+         }
+         else
+         {
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                speed = runSpeed;
+            }
+            else
+            {
+                speed = walkSpeed;
+            }
+         }
+    }
+
+    bool CanGetUp()
+    {
+        Ray groundRay = new Ray(transform.position, transform.up);
+        RaycastHit groundHit;
+
+        if (Physics.SphereCast(groundRay, charController.radius + 0.05f, out groundHit, rayDistance, groundLayer))
+        {
+            if (Vector3.Distance(transform.position, groundHit.point) < 2.3f)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    IEnumerator MoveCameraCrouch()
+    {
+        charController.height = is_Crouching ? default_ControllerHeight / 1.5f : default_ControllerHeight;
+        charController.center = new Vector3(0f, charController.height / 2f, 0f);
+
+        camHeight = is_Crouching ? default_CamPos.y / 1.5f : default_CamPos.y;
+
+        // while we're crouching
+        while (Mathf.Abs(camHeight - firstPerson_View.localPosition.y) > 0.01f)
+        {
+            firstPerson_View.localPosition = Vector3.Lerp(firstPerson_View.localPosition,
+            new Vector3(default_CamPos.x, camHeight, default_CamPos.z),
+            Time.deltaTime * 11f);
+
+            yield return null;
+        }
+        
+
     }
 }
